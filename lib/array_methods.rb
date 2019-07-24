506773e90b782780f6859c8f58c12a26665bac9d
Eugenia Kim <elk303@nyu.edu>
2019-07-24 03:40:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
  def find_element_index(array, value_to_find) 
-   array.length.times do |index| 
-     if array[index] == value_to_find 
-   return index 
- end
-end
-end 
+    array.length.times do |index| 
+      if array[index] == value_to_find 
+    return index 
+    end
+  end 
 return nil  
 end
 
