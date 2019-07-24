92b459e86a6f8f3c8e195ac836bf0e6cbff7ba1e
Eugenia Kim <elk303@nyu.edu>
2019-07-24 03:19:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+ def find_element_index(array, value_to_find) 
+   array.length.times do |index| 
+     if array[index] == value_to_find 
+   return array[index] 
+   end
+ end
 
 def find_max_value(array)
-  # Add your solution here
+  array = array.minmax
+  array[1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array = array.minmax
+  array[0]
 end
